4ceb3d7ee0a987dbe255094d716eb356a74630f5
Gjorgj Kushi <robertmari@MacBook-Air-di-Robert.local>
2025-11-02 16:09:00 +0000

    button change

--- app/_components/Button.js
+++ app/_components/Button.js
@@ -2,9 +2,9 @@ function Button({ onClick, children }) {
   return (
     <button
       onClick={onClick}
-      className="cursor-pointer font-semibold overflow-hidden relative z-100 border border-primary-700 group px-8 py-2"
+      className="cursor-pointer font-semibold  rounded-md overflow-hidden relative z-100 border bg-primary-700 border-primary-700 group px-8 py-2"
     >
-      <span className="relative z-10 text-primary-700 group-hover:text-white text-xl font-bold duration-500">
+      <span className="relative z-10 text-white bg-primary-700 text-xl font-bold duration-500">
         {children}
       </span>
       <span className="absolute w-full h-full bg-primary-700 -left-32 top-0 -rotate-45 group-hover:rotate-0 group-hover:left-0 duration-500"></span>
